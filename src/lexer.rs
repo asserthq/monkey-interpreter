9f3e8ffa73446d448b16f7b84fcd1ca6240ac08e
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-02-02 01:37:16 +0700

    refactor change &str to String & add test for multi-character tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -14,12 +14,18 @@ pub enum Token {
     Assign,
     Plus,
     Minus,
+
+    Let,
+    Function,
+
+    Ident(String),
+    Int(String)
 }
 
 impl Token {
-    pub fn from_string(input: &str) -> Token {
+    pub fn from_string(input: String) -> Token {
         use Token::*;
-        match input {
+        match input.as_str() {
             "(" => LParen,
             ")" => RParen,
             "{" => LBrace,
@@ -37,27 +43,30 @@ impl Token {
     }
 }
 
-pub struct Lexer<'a> {
-    input: &'a str,
+pub struct Lexer{
+    input: String,
     pos: usize,
     sym: Option<char>,
 }
 
-impl<'a> Lexer<'a> {
-    pub fn new(input: &'a str) -> Self {
+impl Lexer {
+    pub fn new(input: &str) -> Self {
         Self {
-            input,
+            input: input.to_string(),
             pos: 0,
             sym: input.chars().next(),
         }
     }
 
     pub fn next_token(&mut self) -> Token {
-        if self.pos >= self.input.len() {
-            return Token::Eof;
-        }
-        let tok = Token::Unknown;
+        let tok = match self.sym {
+            None => Token::Eof,
+            Some(sym) => {
+                Token::from_string(sym.to_string())
+            }
+        };
         self.pos += 1;
+        self.sym = self.next_sym();
         tok
     }
 
@@ -92,4 +101,92 @@ mod tests {
         while lexer.next_token() != Token::Eof {}
         assert_eq!(lexer.next_token(), Token::Eof);
     }
+
+    #[test]
+    fn tokenize_single_character_tokens() {
+        use Token::*;
+        let input = "a=5;({ })+-,";
+        let check = vec![
+            Unknown,
+            Assign, 
+            Unknown,
+            Semicolon,
+            LParen,
+            LBrace,
+            Unknown,
+            RBrace,
+            RParen,
+            Plus,
+            Minus,
+            Comma
+        ];
+        let mut tokens: Vec<Token> = Vec::with_capacity(12);
+        let mut lexer = Lexer::new(input); 
+        let mut tok = lexer.next_token();
+        while tok != Token::Eof {
+            tokens.push(tok);
+            tok = lexer.next_token();
+        }
+        assert_eq!(tokens, check);
+    }
+
+    #[test]
+    fn tokenize_multi_character_tokens() {
+        use Token::*;
+        let input = r#"
+            let five = 5;
+            let ten = 10;
+            let add = fn(x, y) {
+                x + y;
+            };
+            let result = add(five, ten);
+        "#;
+        let check = vec![
+            Let,
+            Ident("five".into()),
+            Assign,
+            Int("5".into()),
+            Semicolon,
+            Let,
+            Ident("ten".into()),
+            Assign,
+            Int("10".into()),
+            Semicolon,
+            Let,
+            Ident("add".into()),
+            Assign,
+            Function,
+            LParen,
+            Ident("x".into()),
+            Comma,
+            Ident("y".into()),
+            RParen,
+            LBrace,
+            Ident("x".into()),
+            Plus,
+            Ident("y".into()),
+            Semicolon,
+            RBrace,
+            Semicolon,
+            Let,
+            Ident("result".into()),
+            Assign,
+            Ident("add".into()),
+            LParen,
+            Ident("five".into()), 
+            Comma, 
+            Ident("ten".into()),
+            RParen,
+            Semicolon
+        ];
+        let mut tokens: Vec<Token> = Vec::with_capacity(40);
+        let mut lexer = Lexer::new(input); 
+        let mut tok = lexer.next_token();
+        while tok != Token::Eof {
+            tokens.push(tok);
+            tok = lexer.next_token();
+        }
+        assert_eq!(tokens, check);
+    }
+    
 }
